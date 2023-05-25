c25a3fde96bacd73ce6ff961d377e53e71fff7c0
MingxiaoZhang <m468zhan@uwaterloo.ca>
2023-05-24 22:28:19 -0400

    added entry program

---
+++ movies_api/app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv('.env')
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+mysql = MySQL(app)
+
+
+# Route to check database connection
+@app.route('/')
+def check_database_connection():
+    try:
+        # Attempt to connect to the database
+        mysql.connection.ping()
+        return 'Successfully connected to the database!'
+    except Exception as e:
+        return f'Failed to connect to the database: {str(e)}'
+
+
+if __name__ == '__main__':
+    app.run()
