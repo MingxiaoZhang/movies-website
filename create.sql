dde6d2373472558e284d674eaf07a1861b37f53d
MingxiaoZhang <m468zhan@uwaterloo.ca>
2023-06-13 20:20:34 -0400

    updated sql tables

--- create.sql
+++ create.sql
@@ -1,11 +1,46 @@
-CREATE SCHEMA imdb_web;
-CREATE TABLE imdb_web.WorkInfo (
-	wid INT NOT NULL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    year INT NOT NULL
+CREATE SCHEMA movies;
+CREATE TABLE movies.basic_info (
+	movie_id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(255),
+    start_year INT,
+    run_time_minutes INT,
+    genres VARCHAR(255),
+    is_adult TINYINT(1)
 );
-CREATE TABLE imdb_web.Ratings (
-	wid INT NOT NULL,
-    rating DECIMAL(3, 1),
-    num_participants INT DEFAULT 0
-)
+CREATE TABLE movies.person_info (
+	person_id INT NOT NULL PRIMARY KEY,
+    primary_name VARCHAR(100),
+    birth_year INT,
+    death_year INT,
+    primary_profession VARCHAR(100),
+    known_for_titles VARCHAR(100)
+);
+CREATE TABLE movies.movie_rating (
+	movie_id INT NOT NULL,
+    average_rating DECIMAL(3, 1),
+    num_votes INT,
+    FOREIGN KEY (movie_id) REFERENCES movies.basic_info(movie_id)
+);
+CREATE TABLE movies.movie_director (
+	movie_id INT NOT NULL,
+    person_id INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies.basic_info(movie_id),
+    FOREIGN KEY (person_id) REFERENCES movies.person_info(person_id)
+);
+CREATE TABLE movies.movie_actor (
+	movie_id INT NOT NULL,
+    person_id INT NOT NULL,
+    characters_played VARCHAR(255),
+    FOREIGN KEY (movie_id) REFERENCES movies.basic_info(movie_id),
+    FOREIGN KEY (person_id) REFERENCES movies.person_info(person_id)
+);
+CREATE TABLE movies.genres (
+	genre_id INT NOT NULL PRIMARY KEY,
+    genre_name VARCHAR(100)
+);
+CREATE TABLE movies.movie_genres (
+	movie_id INT NOT NULL,
+    genre_id INT NOT NULL,
+	FOREIGN KEY (movie_id) REFERENCES movies.basic_info(movie_id),
+    FOREIGN KEY (genre_id) REFERENCES movies.genres(person_id)
+);
---
+++ script.py
@@ -0,0 +1,269 @@
+import mysql.connector
+
+# MySQL database connection details
+host = 'localhost'
+user = 'root'
+password = 'ZMx-02524'
+database = 'imdb_web'
+
+# Chunk size for processing the file
+chunk_size = 100  # Adjust as needed
+
+
+def import_basics():
+    # TSV file path
+    tsv_file = '../basics.tsv'
+
+    # Establish a connection to MySQL
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
+
+    # Create a cursor object
+    cursor = connection.cursor()
+
+    # Truncate the table before importing data (optional)
+    truncate_query = "TRUNCATE TABLE basics"
+    cursor.execute(truncate_query)
+
+    # Open the TSV file for reading
+    with open(tsv_file, 'r', encoding='utf-8') as file:
+        # Skip the header line
+        next(file)
+
+        # Read and process the file in chunks
+        chunk = []
+
+        for line in file:
+            # Split the line into columns
+            columns = line.strip().split('\t')
+
+            # Append the columns to the chunk
+            if columns[1] == 'movie' and columns[5] != '\\N' and columns[7] != '\\N' and columns[8] != '\\N':
+                # Append the columns to the chunk
+                chunk.append([columns[2], (columns[4] == '1'), int(columns[5]), int(columns[7]),
+                              columns[8], int(columns[0][2:])])
+
+            # Check if the chunk has reached the desired size
+            if len(chunk) >= chunk_size:
+                print(chunk[0])
+                # Load the chunk into the table
+                load_query = '''INSERT INTO basics (tconst, primaryTitle, isAdult, startYear, runTimeMinutes, genres)
+                SELECT tconst, %s, %s, %s, %s, %s
+                FROM ratings
+                WHERE tconst = %s'''
+                cursor.executemany(load_query, chunk)
+                connection.commit()
+
+                # Clear the chunk
+                chunk = []
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+def import_casts():
+    # TSV file path
+    tsv_file = '../casts.tsv'
+
+    # Establish a connection to MySQL
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
+
+    # Create a cursor object
+    cursor = connection.cursor()
+
+    # Truncate the table before importing data (optional)
+    # truncate_query = "TRUNCATE TABLE casts"
+    # cursor.execute(truncate_query)
+
+    select_query = "SELECT tconst FROM basics"
+    cursor.execute(select_query)
+    rows = cursor.fetchall()
+    movies = [item for t in rows for item in t]
+
+    # Open the TSV file for reading
+    with open(tsv_file, 'r', encoding='utf-8') as file:
+        # Skip the header line
+        next(file)
+
+        # Read and process the file in chunks
+        chunk = []
+    
+        for line in file:
+            # Split the line into columns
+            columns = line.strip().split('\t')
+
+            # Append the columns to the chunk
+            if 1979388 <= int(columns[0][2:]) <= 2024432 and (int(columns[0][2:]) in movies) and (columns[3] == 'actor' or columns[3] == 'actress') and columns[5] != '\\N':
+                # Append the columns to the chunk
+                print([int(columns[0][2:]), int(columns[2][2:]), columns[5]])
+                load_query = 'INSERT INTO casts (tconst, nconst, characters) VALUES (%s, %s, %s)'
+                cursor.execute(load_query, [int(columns[0][2:]), int(columns[2][2:]), columns[5]])
+                connection.commit()
+
+            # Check if the chunk has reached the desired size
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+def import_names():
+    # TSV file path
+    tsv_file = '../names.tsv'
+
+    # Establish a connection to MySQL
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
+
+    # Create a cursor object
+    cursor = connection.cursor()
+
+    select_query = "SELECT DISTINCT nconst FROM directors"
+    cursor.execute(select_query)
+    rows = cursor.fetchall()
+    directors = [item for t in rows for item in t]
+
+    select_query = "SELECT DISTINCT nconst FROM casts"
+    cursor.execute(select_query)
+    rows = cursor.fetchall()
+    actors = [item for t in rows for item in t]
+
+    # Truncate the table before importing data (optional)
+    truncate_query = "TRUNCATE TABLE names"
+    cursor.execute(truncate_query)
+
+    # Open the TSV file for reading
+    with open(tsv_file, 'r', encoding='utf-8') as file:
+        # Skip the header line
+        next(file)
+
+        # Read and process the file in chunks
+        chunk = []
+
+        for line in file:
+            # Split the line into columns
+            columns = line.strip().split('\t')
+
+            # Append the columns to the chunk
+            if columns[2] != '\\N' and (int(columns[0][2:]) in directors or int(columns[0][2:]) in actors):
+                # Append the columns to the chunk
+                if columns[3] == '\\N':
+                    print([int(columns[0][2:]), columns[1], columns[2], None, columns[4], columns[5]])
+                    load_query = 'INSERT INTO names (nconst, primaryName, birthYear, deathYear, primaryProfession, knownForTitles) VALUES (%s, %s, %s, %s, %s, %s)'
+                    cursor.execute(load_query, [int(columns[0][2:]), columns[1], columns[2], None, columns[4], columns[5]])
+                    connection.commit()
+                else:
+                    print([int(columns[0][2:]), columns[1], columns[2], columns[3], columns[4], columns[5]])
+                    load_query = 'INSERT INTO names (nconst, primaryName, birthYear, deathYear, primaryProfession, knownForTitles) VALUES (%s, %s, %s, %s, %s, %s)'
+                    cursor.execute(load_query,
+                                   [int(columns[0][2:]), columns[1], columns[2], columns[3], columns[4], columns[5]])
+                    connection.commit()
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+def import_ratings():
+    # TSV file path
+    tsv_file = '../ratings.tsv'
+
+    # Establish a connection to MySQL
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
+
+    # Create a cursor object
+    cursor = connection.cursor()
+
+    # Truncate the table before importing data (optional)
+    truncate_query = "TRUNCATE TABLE ratings"
+    cursor.execute(truncate_query)
+
+    # Open the TSV file for reading
+    with open(tsv_file, 'r', encoding='utf-8') as file:
+        # Skip the header line
+        next(file)
+
+        # Read and process the file in chunks
+        chunk = []
+
+        for line in file:
+            # Split the line into columns
+            columns = line.strip().split('\t')
+
+            # Append the columns to the chunk
+            if (int(columns[2]) > 100000):
+                chunk.append([int(columns[0][2:]), float(columns[1]), int(columns[2])])
+
+            # Check if the chunk has reached the desired size
+            if len(chunk) >= chunk_size:
+                print(chunk[0])
+                # Load the chunk into the table
+                load_query = '''
+                INSERT INTO ratings (tconst, averageRating, numVotes) 
+                VALUES (%s, %s, %s)
+                '''
+                cursor.executemany(load_query, chunk)
+                connection.commit()
+
+                # Clear the chunk
+                chunk = []
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+def import_directors():
+    # TSV file path
+    tsv_file = '../directors.tsv'
+
+    # Establish a connection to MySQL
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database)
+
+    # Create a cursor object
+    cursor = connection.cursor()
+
+    # Truncate the table before importing data (optional)
+    truncate_query = "TRUNCATE TABLE directors"
+    cursor.execute(truncate_query)
+
+    # Open the TSV file for reading
+    with open(tsv_file, 'r', encoding='utf-8') as file:
+        # Skip the header line
+        next(file)
+
+        # Read and process the file in chunks
+        chunk = []
+
+        for line in file:
+            # Split the line into columns
+            columns = line.strip().split('\t')
+
+            # Append the columns to the chunk
+            if columns[1] != '\\N':
+                # Append the columns to the chunk
+                id_list = columns[1].split(',')
+                for director_id in id_list:
+                    chunk.append([int(director_id[2:]), int(columns[0][2:])])
+
+            # Check if the chunk has reached the desired size
+            if len(chunk) >= chunk_size:
+                print(chunk[0])
+                # Load the chunk into the table
+                load_query = '''
+                INSERT INTO directors (tconst, nconst)
+                SELECT tconst, %s
+                FROM basics
+                WHERE tconst = %s
+                '''
+                cursor.executemany(load_query, chunk)
+                connection.commit()
+
+                # Clear the chunk
+                chunk = []
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+import_names()
