3e6a72eb8afe574e67a80e2c77f08d83bd89da97
Emily Chen <e94chen@uwaterloo.ca>
2023-07-11 19:45:26 -0400

    milestone 2

---
+++ test-sample/test-production.sql
@@ -0,0 +1,12 @@
+# run import.sql first
+SELECT movie_id, title, start_year, run_time_minutes, is_adult 
+FROM movies.basic_info WHERE title="Nosferatu";
+SELECT person_id,primary_name, birth_year, death_year, primary_profession, known_for_titles 
+FROM movies.person_info WHERE primary_name="Bob";
+SELECT * FROM movies.basic_info ORDER BY title LIMIT 20;
+SELECT * FROM movies.basic_info ORDER BY start_year LIMIT 20;
+SELECT movie_id, title, start_year, run_time_minutes, is_adult 
+FROM movies.basic_info NATURAL JOIN movies.movie_rating 
+ORDER BY average_rating LIMIT 20;
+SELECT * FROM movies.user;
+SELECT * FROM comment WHERE movie_id = NewUser1ORDER BY comment_id DESC LIMIT 5
