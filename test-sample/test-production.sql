9287540026c04e19399c1f9f40c53df7be4f531c
Emily Chen <e94chen@uwaterloo.ca>
2023-07-11 20:14:34 -0400

    add test-production.out

--- test-sample/test-production.sql
+++ test-sample/test-production.sql
@@ -9,4 +9,4 @@ SELECT movie_id, title, start_year, run_time_minutes, is_adult
 FROM movies.basic_info NATURAL JOIN movies.movie_rating 
 ORDER BY average_rating LIMIT 20;
 SELECT * FROM movies.user;
-SELECT * FROM comment WHERE movie_id = NewUser1ORDER BY comment_id DESC LIMIT 5
+SELECT * FROM comment WHERE movie_id = NewUser1 ORDER BY comment_id DESC LIMIT 5
